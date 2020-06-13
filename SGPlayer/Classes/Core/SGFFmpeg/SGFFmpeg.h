d2b2965485d76f3ff48820b1421c2443146d8d55
qiudaomao <qiudaomao@gmail.com>
2020-06-13 18:26:08 +0800

    fix ffmpeg header

--- SGPlayer/Classes/Core/SGFFmpeg/SGFFmpeg.h
+++ SGPlayer/Classes/Core/SGFFmpeg/SGFFmpeg.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
-#import "avformat.h"
-#import "imgutils.h"
-#import "swresample.h"
-#import "swscale.h"
+#import "libavformat/avformat.h"
+#import "libavutil/imgutils.h"
+#import "libswresample/swresample.h"
+#import "libswscale/swscale.h"
 #pragma clang diagnostic pop
 
 void SGFFmpegSetupIfNeeded(void);
